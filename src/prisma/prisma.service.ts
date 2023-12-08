523dccd769c61ad9dd05fb449463e82e4b07738a
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-08 00:43:49 -0300

    integrando mongodb

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { EventEmitter } from 'events';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly eventEmitter = new EventEmitter();
 
-    async onModuleInit(){
+  async onModuleInit() {
+    await this.$connect();
+  }
 
-    }
+  enableShutdownHooks(app: any): void {
+    this.eventEmitter.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
 }
