b3a74d597eff180fd85928af50bed4d055b8dfd2
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-09 17:32:14 -0300

    funcionando mudar uma ordem

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { InitTransactionDto, executeTransactionDto } from './dto/order.dto';
+import { InitTransactionDto, executeTransactionDto,  } from './dto/order.dto'; // Certifique-se de importar o DTO corretamente
 
 @Controller('wallets/:wallet_id/orders')
 export class OrdersController {
@@ -8,17 +8,24 @@ export class OrdersController {
 
   @Post()
   initTransaction(
-    @Param('wallet_id') wallet_id,
-    @Body() body: InitTransactionDto,
-  ) {
+    @Param('wallet_id') walletId: string,
+    @Body() body: Omit<InitTransactionDto, 'wallet_id'>,
+  ): Promise<any> {
     return this.ordersService.startTransaction({
       ...body,
-      wallet_id,
+      wallet_id: walletId,
     });
   }
 
   @Post('execute')
-  executeTransaction(@Body() body: Omit<executeTransactionDto, 'wallet_id'>) {
+  executeTransaction(
+    @Body() body: Omit<executeTransactionDto, 'wallet_id'>,
+  ): Promise<any> {
     return this.ordersService.executeTransaction(body);
+  }
+
+  @Get()
+  all(@Param('wallet_id') walletId: string) {
+    return this.ordersService.all({ wallet_id: walletId });
   }
 }
